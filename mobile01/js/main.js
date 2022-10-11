b3aa52869458af01f39b11a126990b419a51b7c6
choi7ho <113654003+choi7ho@users.noreply.github.com>
2022-10-11 21:53:17 +0900

    Add files via upload

---
+++ mobile01/js/main.js
@@ -0,0 +1,8 @@
+
+// $('.mopen').on('click', function () {
+//     $('.gnb').toggleClass('on')
+// })
+document.querySelector('.mopen').addEventListener('click', function () {
+    document.querySelector('.gnb').classList.toggle('on');
+    this.classList.toggle('on');
+})
